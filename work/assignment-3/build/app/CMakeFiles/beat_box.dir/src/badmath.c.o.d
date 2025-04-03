fe46212f6855675b172633983042e9ee44849ed6
Aki <akkarachaiwangcharoensap@hotmail.com>
2025-04-03 11:46:56 -0700

    Removed: build folders

--- work/assignment-3/build/app/CMakeFiles/beat_box.dir/src/badmath.c.o.d
+++
@@ -1,4 +0,0 @@
-app/CMakeFiles/beat_box.dir/src/badmath.c.o: \
- /home/aki/cmpt-433/work/assignment-3/app/src/badmath.c \
- /usr/include/stdc-predef.h \
- /home/aki/cmpt-433/work/assignment-3/app/include/badmath.h
